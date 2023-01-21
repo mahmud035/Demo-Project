a1d6d2167182ecd0b9ba7fd8f184affc0822bd95
mahmud035 <mahamudulhasan4148@gmail.com>
2023-01-21 14:55:08 +0600

    added navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import logo from '../assets/images/logo.svg';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <div className="px-4 py-6 max-w-screen-xl mx-auto  md:px-24 lg:px-8">
+    <div className="px-4 py-5 max-w-screen-xl mx-auto  md:px-24 lg:px-8">
       <div className="mx-auto container">
         <div className="relative flex items-center justify-between">
           <a
@@ -13,24 +14,11 @@ const Navbar = () => {
             title="Company"
             className="inline-flex items-center"
           >
-            <svg
-              className="w-8 text-deep-purple-accent-400"
-              viewBox="0 0 24 24"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeMiterlimit="10"
-              stroke="currentColor"
-              fill="none"
-            >
-              <rect x="3" y="1" width="7" height="12" />
-              <rect x="3" y="17" width="7" height="6" />
-              <rect x="14" y="1" width="7" height="6" />
-              <rect x="14" y="11" width="7" height="12" />
-            </svg>
-            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-              Company
-            </span>
+            <img
+              className="w-48 h-10 object-cover rounded-r-full"
+              src={logo}
+              alt=""
+            />
           </a>
           <ul className="items-center hidden space-x-8 lg:flex">
             <li>
@@ -107,24 +95,11 @@ const Navbar = () => {
                         title="Company"
                         className="inline-flex items-center"
                       >
-                        <svg
-                          className="w-8 text-deep-purple-accent-400"
-                          viewBox="0 0 24 24"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeMiterlimit="10"
-                          stroke="currentColor"
-                          fill="none"
-                        >
-                          <rect x="3" y="1" width="7" height="12" />
-                          <rect x="3" y="17" width="7" height="6" />
-                          <rect x="14" y="1" width="7" height="6" />
-                          <rect x="14" y="11" width="7" height="12" />
-                        </svg>
-                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                          Company
-                        </span>
+                        <img
+                          className="w-48 h-10 object-cover rounded-r-full"
+                          src={logo}
+                          alt=""
+                        />
                       </a>
                     </div>
                     <div>
